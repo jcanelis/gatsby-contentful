3e8e7cc415460838bf0b93d16420ec9aeb2b7efa
John Canelis <jcanelis@gmail.com>
2021-06-27 03:03:00 -0400

    usa map

--- src/components/map.js
+++ src/components/map.js
@@ -1,75 +1,41 @@
 import * as React from "react";
-// import { geoEqualEarth, geoPath } from "d3-geo";
 import * as d3 from "d3";
+import * as topojson from "topojson-client";
 import { useD3 } from "../hooks/useD3";
+import { us } from "./usa";
+// import { geoEqualEarth, geoPath } from "d3-geo";
 
 const Map = ({ data }) => {
+  const path = d3.geoPath();
   const ref = useD3(
     (svg) => {
       const height = 500;
       const width = 1000;
       const margin = { top: 20, right: 30, bottom: 30, left: 40 };
 
-      const x = d3
-        .scaleBand()
-        .domain(data.map((d) => d.year))
-        .rangeRound([margin.left, width - margin.right])
-        .padding(0.1);
-
-      const y1 = d3
-        .scaleLinear()
-        .domain([0, d3.max(data, (d) => d.sales)])
-        .rangeRound([height - margin.bottom, margin.top]);
-
-      const xAxis = (g) =>
-        g.attr("transform", `translate(0,${height - margin.bottom})`).call(
-          d3
-            .axisBottom(x)
-            .tickValues(
-              d3
-                .ticks(...d3.extent(x.domain()), width / 40)
-                .filter((v) => x(v) !== undefined)
-            )
-            .tickSizeOuter(0)
-        );
-
-      const y1Axis = (g) =>
-        g
-          .attr("transform", `translate(${margin.left},0)`)
-          .style("color", "#000")
-          .attr("class", "labels")
-          .call(d3.axisLeft(y1).ticks(null, "s"))
-          .call((g) => g.select(".domain").remove())
-          .call((g) =>
-            g
-              .append("text")
-              .attr("x", -margin.left)
-              .attr("y", 10)
-              .attr("fill", "#000")
-              .attr("text-anchor", "start")
-              .text(data.y1)
-          );
-
-      svg.select(".x-axis").call(xAxis);
-      svg.select(".y-axis").call(y1Axis);
+      svg
+        .append("g")
+        .selectAll("path")
+        .data(topojson.feature(us, us.objects.states).features)
+        .join("path")
+        .attr("fill", "rgba(0,0,0,0.1)")
+        .attr("d", path)
+        .append("title")
+        .text("cool");
 
       svg
-        .select(".plot-area")
-        .attr("fill", "rgba(0,0,0,1)")
-        .selectAll(".bar")
-        .data(data)
-        .join("rect")
-        .attr("class", "bar")
-        .attr("x", (d) => x(d.year))
-        .attr("width", x.bandwidth())
-        .attr("y", (d) => y1(d.sales))
-        .attr("height", (d) => y1(0) - y1(d.sales));
+        .append("path")
+        .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
+        .attr("fill", "none")
+        .attr("stroke", "white")
+        .attr("stroke-linejoin", "round")
+        .attr("d", path);
     },
-    [data.length]
+    [50]
   );
 
   return (
-    <svg ref={ref} viewBox="0 0 960 500" preserveAspectRatio="xMidYMid meet">
+    <svg ref={ref} viewBox="0 0 960 700" preserveAspectRatio="xMidYMid meet">
       <g className="plot-area" />
       <g className="x-axis" />
       <g className="y-axis" />
